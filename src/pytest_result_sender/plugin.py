2476b13d15e56aeb12363d0cc4f3624a87118679
whitefox <dongfz@foxmail.com>
2025-09-19 11:47:48 +0800

    pytest插件，创建插件，打包

---
+++ src/pytest_result_sender/plugin.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def pytest_configure():
+
+    #配置加载完毕后执行，测试用例执行之前
+
+    print(f"{datetime.now()} pytest开始执行了")
+
+def pytest_unconfigure():
+    # 配置卸载之后，测试执行后。执行
+    print(f"{datetime.now()} pytest结束执行")
