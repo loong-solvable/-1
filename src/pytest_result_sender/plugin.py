8c280ed9c00abdf8d6c0f9b979ae11619ac80620
whitefox <dongfz@foxmail.com>
2025-09-19 14:37:51 +0800

    使用代码格式工具进行了格式的质量控制

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
+
 def pytest_configure():
 
-    #配置加载完毕后执行，测试用例执行之前
+    # 配置加载完毕后执行，测试用例执行之前
 
     print(f"{datetime.now()} pytest开始执行了")
+
 
 def pytest_unconfigure():
     # 配置卸载之后，测试执行后。执行
